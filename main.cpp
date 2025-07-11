9cd8746a1070027ee369b7ee1f871b9a82dbcb4c
aXXo-dev <hunters12@protonmail.com>
2025-07-11 10:34:33 -0400

    feat: UI almost done

--- main.cpp
+++ main.cpp
@@ -1,89 +1,13 @@
-﻿#include <string>
-#include <thread>
+﻿#include <thread>
 #include <chrono>
 #include <array>
-#include <iostream>
 
-#include <ftxui/component/component.hpp>
-#include <ftxui/component/screen_interactive.hpp>
-#include <ftxui/dom/elements.hpp>
-
+#include "ui.h"
 #include "antidebug.h"
-
-//
-// [SECTION] Defines
-//
-
-static std::atomic<bool> running{true};
-
-//
-// [SECTION] Functions
-//
-
-void uiRoutine(AntiDebug::AntiDebugOptions& options)
-{
-    using namespace ftxui;
-
-    std::array<bool, ANTIDBG_OPTS_COUNT> checkbox_states;
-    std::vector<Component> checkboxes;
-
-    for (int i{}; i < options.size(); i++)
-    {
-        checkbox_states[i] = options[i].enabled.load();
-
-        CheckboxOption option;
-        option.label = options[i].name;
-        option.checked = &checkbox_states[i];
-        checkboxes.push_back(Checkbox(option));
-    }
-
-    auto checkbox_container{ Container::Vertical(checkboxes) };
-
-    auto screen{ ScreenInteractive::Fullscreen() };
-    auto component = Renderer(checkbox_container, [&] {
-        for (int i{}; i < options.size(); i++)
-            options[i].enabled.store(checkbox_states[i]);
-
-        Elements checkbox_elements;
-        for (int i{}; i < options.size(); i++)
-        {
-            auto status{ options[i].detected ? text(" [DETECTED]") | color(Color::Red) : text("") };
-            checkbox_elements.push_back(
-                hbox({
-                    checkboxes[i]->Render(),
-                    status
-                    })
-            );
-        }
-
-        return vbox({
-            text("AntiDebug - Haxo Games Inc.") | bold | center,
-            separator(),
-            vbox(checkbox_elements),
-            separator(),
-            text("Use arrow keys to navigate, Space to toggle, Q to quit") | dim
-        }) | border;
-    });
-
-    component |= CatchEvent([&](Event event) {
-        if (event == Event::Character('q') || event == Event::Character('Q')) 
-        {
-            running = false;
-            screen.ExitLoopClosure()();
-            return true;
-        }
-
-        return false;
-    });
-
-    screen.Loop(component);
-}
 
 int main()
 {
     using namespace AntiDebug;
-
-    SetConsoleTitleA("AntiDebug");
 
 	AntiDebugOptions options{
 		AntiDebugOption("IsDebuggerPresent", true, callbackIsDebuggerPresent),
@@ -92,11 +16,26 @@ int main()
         AntiDebugOption("NtQueryInformationProcess", true, callbackIsDebuggerPresent),
 	};
 
-    std::thread ui_thread([&options] { uiRoutine(options); });
-    ui_thread.join();
+    SetConsoleTitleA("AntiDebug");
+    std::thread ui_thread([&options] { UI::routine(options); });
 
-    while (running)
+    while (UI::running)
     {
+        for (auto& option : options)
+        {
+            if (option.enabled)
+            {
+                bool was_detected{ option.detected };
+                option.callback(option);
+                bool is_detected{ option.detected };
+
+                if (is_detected && !was_detected || !is_detected && was_detected)
+                    UI::screen.PostEvent(ftxui::Event::Custom);
+            }
+            else
+                option.detected = false;
+        }
+
         std::this_thread::sleep_for(std::chrono::milliseconds(25));
     }
 
